4d296805818f8f8dd5a1dc74507e5fdfe59318f8
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2025-01-25 09:29:38 +0000

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -112,6 +112,7 @@ const Dashboard = () => {
 
   return (
     <div className="flex flex-1">
+      {newMessageModalShowing && <></>}
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex flex-col gap-2 justify-center items-center">
           <CustomerDataTable />
