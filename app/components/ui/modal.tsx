243c9404d040e84c9a33b8b4b0a148f53debe7d4
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2025-01-27 09:11:51 +0000

    Universal modal

--- app/components/ui/modal.tsx
+++ app/components/ui/modal.tsx
@@ -1,10 +1,12 @@
+import { X } from "lucide-react"
 import { useEffect } from "react"
 
 interface ModalProps {
     onClose: (closed: boolean) => void
+    children?: React.ReactNode
 }
 
-export const Modal = ({onClose}: ModalProps) => {
+export const Modal = ({ onClose, children }: ModalProps) => {
 
     useEffect(() => {
         const handleEscapeKey = (event: KeyboardEvent) => {
@@ -18,14 +20,20 @@ export const Modal = ({onClose}: ModalProps) => {
 
         // Clean up by removing listener when component unmounts
         return () => {
-            document.removeEventListener('keydown', handleEscapeKey) 
+            document.removeEventListener('keydown', handleEscapeKey)
         }
     }, [onClose])
 
 
     return (
-        <div className="absolute top-0 left-0 bg-black transparent p-4 w-screen h-screen z-1000 flex items-center justify-center">
-            <div className="w-[40rem] h-[40rem] dark:bg-neutral-900 opacity">
+        <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+            <div className="w-[80rem] h-4/5 dark:bg-neutral-900 bg-white rounded-lg shadow-xl">
+                <div className="flex justify-end p-4 items-center">
+                    <button onClick={() => onClose(false)} className="hover:bg-white p-2 rounded-lg hover:text-black">
+                        <X />
+                    </button>
+                </div>
+                {children}
             </div>
         </div>
     )
